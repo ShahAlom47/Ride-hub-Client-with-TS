af24e44b1fbf5c751764370a534226e83f41258f
ShahAlom47 <sahalom4729@gmail.com>
2024-10-02 14:22:52 +0400

    update product details modal  and modal data

--- src/Pages/Shop/ProductDetails/ProductDetails.tsx
+++ src/Pages/Shop/ProductDetails/ProductDetails.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { Products } from "../Shop";
 import useAxiosPublic from "../../../CustomHocks/useAxiosPublic";
 import ReactStars from "react-rating-stars-component";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 
 type DetailsProps = {
@@ -11,6 +13,7 @@ type DetailsProps = {
 const ProductDetails = ({ id }: DetailsProps) => {
 
     const AxiosPublic = useAxiosPublic()
+    const [productQ,setProductQ]=useState(1)
 
     const { data } = useQuery({
         queryKey: ['product_details', id],
@@ -20,15 +23,34 @@ const ProductDetails = ({ id }: DetailsProps) => {
         }
     });
 
+
+    const handelProductQ = (value: string): void => { 
+        if (value === 'decrease' && productQ > 1) {
+            setProductQ(productQ - 1);
+            return;
+        } 
+        else if ( value === 'increase' ) {
+            if( data?.stock && data.stock > productQ){
+            setProductQ(productQ + 1);
+            return
+            }
+            Swal.fire({
+                text:'Not Available More Product Quantity',
+                toast:true
+            })
+        }
+    }
+    
+
     console.log(data);
     return (
         <div className="max-w">
-            <div className=" grid grid-cols-12  max-h-screen bg-color-p">
+            <div className=" grid lg:grid-cols-12 md:grid-cols-12 grid-cols-1  max-h-screen bg-color-p overflow-y-auto">
                 <div className=" col-span-5 p-5">
                     <img src="https://m.media-amazon.com/images/I/61xv8k5p1gL._AC_SL1500_.jpg" alt={` ${data?.name || 'Product Photo'} Photo`} />
                 </div>
 
-                <div className="col-span-7 p-7 space-y-3 ">
+                <div className="col-span-7 p-7 space-y-3 max-h-screen overflow-y-scroll ">
                     <div className=" flex gap-3 items-center">
                         <ReactStars
                             count={5}
@@ -46,7 +68,27 @@ const ProductDetails = ({ id }: DetailsProps) => {
                         <p className="">{data?.stock==0? '(Out of Stock)':`(Available: ${data?.stock} Pcs)`}</p>
 
                     </div>
+                    <p className="text-lg"> {data?.description}</p>
 
+                    <div className="flex gap-4 items-center py-5 text-white">
+
+                        <div className=" flex gap-2 items-center border p-2 py h-10">
+                            <button onClick={()=>handelProductQ('decrease')} className=" bg-gray-700 rounded-full pb-1 h-8 w-8 items-center flex justify-center text-4xl ">-</button>
+                            <p className="text-xl font-bold px-2">{productQ}</p>
+                            <button onClick={()=>handelProductQ('increase')} className="bg-gray-700 rounded-full pb-1 h-8 w-8 items-center flex justify-center text-4xl">+</button>
+                        </div>
+                        <button  className=" btn-p p-4 h-10">ADD TO CART</button>
+                    </div>
+                      <p className=" text-lg"> <span className="font-bold text-white">Brand:  </span>{data?.brand}</p>
+                      <p className=" text-lg"> <span className="font-bold text-white">Category: </span>{data?.category}</p>
+                      <p className=" text-lg"> <span className="font-bold text-white">Color: </span>{data?.color}</p>
+                      <p className=" text-lg"> <span className="font-bold text-white">Material: </span>{data?.material}</p>
+
+
+                      <p className=" text-lg"> <span className="font-bold text-white">Brand:  </span>{data?.brand}</p>
+                      <p className=" text-lg"> <span className="font-bold text-white">Category: </span>{data?.category}</p>
+                      <p className=" text-lg"> <span className="font-bold text-white">Color: </span>{data?.color}</p>
+                      <p className=" text-lg"> <span className="font-bold text-white">Material: </span>{data?.material}</p>
 
 
                 </div>
--- src/SharedComponent/ReactModal/ReactModal.tsx
+++ src/SharedComponent/ReactModal/ReactModal.tsx
@@ -3,18 +3,18 @@ import Modal from 'react-modal';
 import { MdOutlineCancel } from "react-icons/md";
 import { ReactNode } from 'react';
 
-const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        padding:'0px',
-        zIndex:'999'
-    },
-};
+// const customStyles = {
+//     content: {
+//         top: '50%',
+//         left: '50%',
+//         right: 'auto',
+//         bottom: 'auto',
+//         marginRight: '-50%',
+//         transform: 'translate(-50%, -50%)',
+//         padding:'0px',
+//         zIndex:'999'
+//     },
+// };
 interface ModalProps {
     children: ReactNode;
     modalIsOpen: boolean;  // Modal open status
@@ -36,8 +36,8 @@ const ReactModal = ({ children, modalIsOpen, setIsOpen }: ModalProps) => {
                 style={customStyles}
                 contentLabel="Example Modal"
             >
-                <div className="relative z-50 ">
-                    <button onClick={closeModal} className='absolute top-4 right-4 hover:text-color-s text-white text-3xl bg-transparent'>
+                <div className="relative z-50  ">
+                    <button onClick={closeModal} className=' bg-gray-900 bg-opacity-50 rounded-full  absolute top-4 right-4 hover:text-color-s text-white text-3xl bg-transparent'>
                         <MdOutlineCancel />
                     </button>
                     {children}
@@ -48,3 +48,30 @@ const ReactModal = ({ children, modalIsOpen, setIsOpen }: ModalProps) => {
 };
 
 export default ReactModal;
+
+
+const customStyles = {
+    overlay: {
+        zIndex: '50',
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        
+    },
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        padding:'0px',
+        // marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '90%',
+        maxWidth: '90%',
+        minWidth: '80%',
+        height: 'auto',
+        maxHeight: '90vh',
+        // overflow: 'auto',
+        position: 'relative',
+        margin: '10px',
+    
+    },
+};
