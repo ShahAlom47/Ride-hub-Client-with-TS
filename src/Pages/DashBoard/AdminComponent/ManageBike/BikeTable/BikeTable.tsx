d0e96becfdb787dd090a2dbf95c3392b215cbad1
ShahAlom47 <sahalom4729@gmail.com>
2024-12-12 22:50:35 +0600

    Add bike deletion feature to Manage Bike page

--- src/Pages/DashBoard/AdminComponent/ManageBike/BikeTable/BikeTable.tsx
+++ src/Pages/DashBoard/AdminComponent/ManageBike/BikeTable/BikeTable.tsx
@@ -4,16 +4,23 @@ import { Link } from "react-router-dom";
 import { bikeDataType } from "../../../../OurBikes/BikeCard/BikeCard";
 
 import { ResponsiveTable } from "responsive-table-react";
+import { MdDeleteSweep } from "react-icons/md";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../../../CustomHocks/useAxiosSecure";
 
 
 interface PropsType {
     tableData: bikeDataType[];
     refetch: () => void;
 }
+interface ResType {
+
+    success: boolean;
+    message: string
+}
 
 const BikeTable = ({ tableData, refetch }: PropsType) => {
-    console.log(refetch);
-    console.log(tableData);
+    const axiosSecure = useAxiosSecure();
 
     const columns = [
         {
@@ -36,6 +43,10 @@ const BikeTable = ({ tableData, refetch }: PropsType) => {
             "id": "action",
             "text": "Action"
         },
+        {
+            "id": "delete",
+            "text": "Delete"
+        },
     ]
 
     const data = tableData?.map((bike) => ({
@@ -43,9 +54,44 @@ const BikeTable = ({ tableData, refetch }: PropsType) => {
         brand: bike.brand,
         model: bike?.model,
         rentPrice: (<p>$ {bike.rental_price_per_day}</p>),
-        action: (<Link to={`/my-dashBoard/editBike/${bike?._id}`}><button className=" btn-s">View $ Edit</button></Link>)
+        action: (<Link to={`/my-dashBoard/editBike/${bike?._id}`}><button className=" btn-s">View $ Edit</button></Link>),
+        delete: (<button type="button" onClick={() => handleDeleteBike(bike?._id)} className=" text-3xl  text-color-s hover:text-4xl transition-all duration-300"><MdDeleteSweep /></button>)
     }))
 
+
+    const handleDeleteBike = async (id: string): Promise<void> => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+
+                const deleteRes = await axiosSecure.delete<ResType>(`/bikeData/deleteBike/${id}`)
+
+                Swal.fire({
+                    toast: true,
+                    icon: deleteRes?.data.success ? 'success' : 'error',
+                    title: deleteRes?.data.message,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+
+
+                if (deleteRes?.data.success) {
+                    refetch()
+                }
+
+
+            }
+        });
+
+    };
 
 
 
@@ -54,9 +100,9 @@ const BikeTable = ({ tableData, refetch }: PropsType) => {
             <ResponsiveTable
                 columns={columns}
                 data={data}
-               
+
             />
-           
+
         </div>
     );
 };
